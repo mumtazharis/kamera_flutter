57772c768278c4975f28e4930944c23fc6dc0e23
mumtazharis <ahmadmumtazhrs@gmail.com>
2024-11-07 11:10:04 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# kamera_flutter
+
+Ahmad Mumtaz Haris (01) TI_3H
+
+## Hasil praktikum 1
+![alt text](images/Screenshot_20241107-103736_kamera_flutter.png)
+
+![alt text](images/Screenshot_20241107-103740_kamera_flutter.png)
+
+## Hasil tugas praktikum
+![alt text](images/Screenshot_20241107-105350_kamera_flutter.png)
+
+
+#### 3. Jelaskan maksud void async pada praktikum 1?
+Future<void> async pada main() memungkinkan fungsi main untuk menangani operasi tidak sinkron, yaitu inisialisasi plugin dan pengambilan daftar kamera. Hal ini memastikan aplikasi berjalan dengan benar dan kamera tersedia saat dibutuhkan tanpa masalah sinkronisasi.
+#### 4. Jelaskan fungsi dari anotasi @immutable dan @override ?
+- Anotasi @immutable digunakan untuk menandai bahwa sebuah kelas tidak boleh diubah setelah dibuat. Ini berarti bahwa semua atribut dari kelas tersebut harus bersifat final, atau konstan (constant), sehingga tidak bisa diubah setelah nilai awal diberikan melalui konstruktor.
+- Anotasi @override digunakan untuk menunjukkan bahwa suatu metode atau properti pada kelas merupakan implementasi ulang (override) dari metode atau properti yang didefinisikan pada kelas induknya (superclass).
---
+++ lib/widget/filter_carousel.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'filter_selector.dart';
+import 'dart:io';
+
+@immutable
+class PhotoFilterCarousel extends StatefulWidget {
+  final String imagePath;
+  const PhotoFilterCarousel({
+    Key? key,
+    required this.imagePath, // Define imagePath as a required parameter
+  }) : super(key: key);
+
+
+  @override
+  State<PhotoFilterCarousel> createState() => _PhotoFilterCarouselState();
+}
+
+class _PhotoFilterCarouselState extends State<PhotoFilterCarousel> {
+
+  final _filters = [
+    Colors.white,
+    ...List.generate(
+      Colors.primaries.length,
+      (index) => Colors.primaries[(index * 4) % Colors.primaries.length],
+    )
+  ];
+
+  final _filterColor = ValueNotifier<Color>(Colors.white);
+
+  void _onFilterChanged(Color value) {
+    _filterColor.value = value;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.black,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: _buildPhotoWithFilter(),
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: _buildFilterSelector(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPhotoWithFilter() {
+    return ValueListenableBuilder(
+      valueListenable: _filterColor,
+      builder: (context, color, child) {
+        // Anda bisa ganti dengan foto Anda sendiri
+        return Image.file(File(widget.imagePath),
+          color: color.withOpacity(0.5),
+          colorBlendMode: BlendMode.color,
+          fit: BoxFit.cover,
+        );
+      },
+    );
+  }
+
+  Widget _buildFilterSelector() {
+    return FilterSelector(
+      onFilterChanged: _onFilterChanged,
+      filters: _filters,
+    );
+  }
+}
